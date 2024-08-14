243bbbfe8f691a01b92f641af419c7a31d7f9cdc
Mercer011 <wavemercer23@gmail.com>
2024-08-14 21:19:03 +0530

    date 14/8/24

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main(args:Array<String>){
+
+//     val and Var
+
+//     var a = 34
+//     println(a)
+//     var a = 25
+//     print(a)
+
+
+//     intializing
+//     var a: String
+//     a= "Hello"
+//     print(a)
+
+
+
+
+
+}
