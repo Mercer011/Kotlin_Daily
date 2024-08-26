273ec4a7823c65c587acb618828c259affaf7332
Mercer011 <wavemercer23@gmail.com>
2024-08-26 13:40:34 +0530

    Day 5 Control Statement

---
+++ src/main/kotlin/Day5/ControlStatement.kt
@@ -0,0 +1,54 @@
+package Day5
+
+fun main() {
+    //if(condition){
+// }//
+//    val a = 100
+//    val c = 69
+//    val b = 21
+//    if(a> b) print("Dhakkan")
+//
+//    var temp = 68
+//
+//    if(temp > 50)
+//        println("Garam hai Garam hai")
+//    else(println("Chale Chai pine"))
+//
+//
+    val age = 11
+    if (age < 13) {
+        println("you are an Child")
+    } else if (age <= 19 && age >= 13) {
+        println("Teenager")
+    } else {
+        println("Aou are an Adult")
+    }
+    val score = 80
+    if (score >= 90) {
+        println("A")
+    } else if (score >= 85) {
+        println("B")
+    } else if (score >= 70) {
+        println("C")
+    } else {
+        println("D")
+
+    }
+    var x = "*"
+    val q = 5
+    val e = 4
+
+    if (x == "+") {
+        println("Result: ${q + e}")
+    } else if (x == "-") {
+        println("Result: ${q - e}")
+
+    } else if (x == "/") {
+        println("Result: ${q / e}")
+    } else if (x == "*") {
+        println("Result: ${q * e}")
+    } else {
+        println("Chal")
+    }
+
+}
